3e33e44e0df7b745d887417b5673adf1f79c7315
fattoly <i_masami0320@ezweb.ne.jp>
2016-11-30 23:36:17 +0900

    test_php

--- sample.php
+++ sample.php
@@ -5,20 +5,21 @@
 	<title>sample.php</title>
 </head>
 <body>
-<FORM ACTION="sample.php" method="post">
-	<input type="text" name="x">
+<FORM ACTION="sample.php" method="get">
+	<input type="text" name="x" >
 	+
 	<input type="text" name="y">
 	=?
 	<input type="submit" value="送信">
 </FORM>
 <?php
-$x = $_POST["x"];
-$y = $_POST["y"];
-$answer = $x + $y;
-print "計算結果：";
-echo $x."+".$y."=".$answer."\n";
+if(isset($_GET["x"])){
+     $x = $_GET["x"];
+     $y = $_GET["y"];
+     $answer = $x + $y;
+     print "計算結果：";
+     echo $x."+".$y."=".$answer."\n";
+}
 ?>
-
 </body>
 </html>
